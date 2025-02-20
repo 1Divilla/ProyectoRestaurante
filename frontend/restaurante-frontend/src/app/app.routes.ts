484de5b9506f13185f26932f2b0e4291823e49d6
Rruubeenn23 <rubencereceda23@gmail.com>
2025-02-20 19:32:29 +0100

    Ahora si todo

--- frontend/restaurante-frontend/src/app/app.routes.ts
+++ frontend/restaurante-frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { CartaComponent } from './menu/pages/carta/carta.component'; 
+import { ReservationsComponent } from './admin/pages/reservations/reservations.component';
+import { ReservationPageComponent } from './reservation/pages/reservation-page/reservation-page.component';
 
 
 export const appRoutes: Routes = [
@@ -18,9 +20,15 @@ export const appRoutes: Routes = [
   },
   { path: '', redirectTo: '/carta', pathMatch: 'full' },
   {
+    path: 'reservation',
+    component: ReservationPageComponent,
+    loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule)
+  },
+  {
     path: 'auth', // ✅ Nueva ruta para autenticación
     loadChildren: () => import('./register-login/register-login.module').then(m => m.RegisterLoginModule)
   },
+
   {
     path: '**',
     redirectTo: '', // ✅ Redirigir a Home si la ruta no existe
