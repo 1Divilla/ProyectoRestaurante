8e8e01463ad40b12c29d1e76da0cf2e70d47ab6c
Rruubeenn23 <rubencereceda23@gmail.com>
2025-02-26 16:52:33 +0100

    Alex

---
+++ frontend/restaurante-frontend/src/app/account/components/account-screen/account-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-screen',
+  imports: [],
+  templateUrl: './account-screen.component.html',
+  styleUrl: './account-screen.component.css'
+})
+export class AccountScreenComponent {
+
+}
